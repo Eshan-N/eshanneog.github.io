7f4175c83237a0b8e10121972ed61ae47799a680
Eshan-N <eneog@my.waketech.edu>
2025-07-14 22:19:26 +0530

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,11 @@
+function fetchGradeData() {
+  console.log("Fetching grade data...");
+}
+
+function populateGradebook(data) {
+  console.log("Populating gradebook with:", data);
+}
+
+// Just for testing that functions are triggered
+fetchGradeData();
+populateGradebook([]);
